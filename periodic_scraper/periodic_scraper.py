cafd8c5c503752515ed4861c98fec49cf86a8b4b
Robert <r@u1.home>
2021-02-19 03:11:31 +0000

    feat: get bill path numbers to use as unique identifiers/primary key for now

--- periodic_scraper/periodic_scraper.py
+++ periodic_scraper/periodic_scraper.py
@@ -6,6 +6,8 @@ from parlpy.bills.bill_list_fetcher import BillsOverview
 import mysql.connector
 from mysql.connector.constants import ClientFlag
 
+from collections import namedtuple
+
 # todo: put these in utility file for investigating database
 def print_tables_in_db(cursor):
     cursor.execute("SHOW tables IN bills_app_db")
@@ -24,6 +26,19 @@ def print_all_rows_of_bills_table(cursor):
     for x in cursor:
         print(x)
 
+def insert_all_bill_overview_data(cursor, bill_data):
+    for b in bill_data.itertuples():
+        print(b)
+        print(type(b))
+        # we use the govt site provided path number as id for now
+        bill_detail_path_number = int(getattr(b, "bill_detail_path").rsplit("/")[-1])
+        #print("int bill id: {}".format(bill_detail_path_number))
+
+        bill_name = getattr(b, "bill_title")
+        #cursor.execute("INSERT INTO bills_app_db.Bills (billID) VALUES (334)")
+
+    #conn.commit()
+
 sql_config = {
     "user": "root",
     "password": "",
@@ -40,18 +55,23 @@ conn = mysql.connector.connect(**sql_config)
 cursor = conn.cursor()
 
 # list_tables_in_db(cursor)
-#print_bills_table_structure(cursor)
+# print_bills_table_structure(cursor)
 
-cursor.execute("INSERT INTO bills_app_db.Bills (billID) VALUES (334)")
-conn.commit()
+bills_this_session = BillsOverview()
+bills_this_session.update_all_bills_in_session()
+
+print(bills_this_session.bills_overview_data.columns)
+
+insert_all_bill_overview_data(cursor, bills_this_session.bills_overview_data)
+
+
 
 print_all_rows_of_bills_table(cursor)
 
 cursor.close()
 conn.close()
 
-#bills_this_session = BillsOverview()
-#bills_this_session.update_all_bills_in_session()
+
 
 #print(bills_this_session.bills_overview_data)
 
