6d9690c7a26fa962e98f2e5094e86b9f06ea2399
Robert <1robertchambers@gmail.com>
2021-03-20 00:29:13 +0000

    feat: improved printing for debugging

--- periodic_scraper/periodic_scraper.py
+++ periodic_scraper/periodic_scraper.py
@@ -10,6 +10,7 @@ import mysql.connector
 from mysql.connector.constants import ClientFlag
 
 import pandas as pd
+import os
 
 public_ip = "35.190.194.63"
 
@@ -76,16 +77,13 @@ def execute_mp_data_in_db(cursor, conn, first_name, second_name, member_id, part
 # ('photoPath', b'text', 'YES', '', None, '')
 # ('phoneNum', b'text', 'YES', '', None, '')
 # ('area', b'text', 'YES', '', None, '')
-def insert_mp_data(conn, cursor):
-    mp_fetcher = mf.MPOverview()
+def insert_mp_data(conn, cursor, current_mp_data):
 
-    mp_fetcher.get_all_members(params={"House": "Commons"})
-    current_mp_data = mp_fetcher.mp_overview_data
     pd.set_option("display.max_columns", len(current_mp_data.columns))
 
     print(current_mp_data)
 
-    clear_table(conn, cursor, "MP")
+    #clear_table(conn, cursor, "MP")
 
     for (index,
         name_display,
@@ -116,14 +114,37 @@ def execute_party_data_in_db(cursor, party_id, party_name):
 # clear table, execute insertions and commit Party data
 # todo: write update functionality rather than delete -> add
 def insert_party_data(conn, cursor):
-    clear_table(conn, cursor, "Party")
-
     party_details_list = pf.get_all_parties()
+    max_id_in_use = 0
+    for i in party_details_list:
+        if i.party_id > max_id_in_use:
+            max_id_in_use = i.party_id
+    total_parties_ids_upper_estimate = 4000 + max_id_in_use
 
+    party_details_dict = {}
     for p in party_details_list:
-        execute_party_data_in_db(cursor, p.party_id, p.party_name)
+        party_details_dict[p.party_id] = p.party_name
+
+    for i in range(total_parties_ids_upper_estimate):
+        if i in party_details_dict.keys():
+            execute_party_data_in_db(cursor, i, party_details_dict[i])
+        else:
+            execute_party_data_in_db(cursor, i, "unknown")
 
     conn.commit()
+
+
+def refresh_mp_and_party_tables(conn, cursor):
+    clear_table(conn, cursor, "MP")
+    clear_table(conn, cursor, "Party")
+
+    mp_fetcher = mf.MPOverview()
+
+    mp_fetcher.get_all_members(params={"House": "Commons"})
+    current_mp_data = mp_fetcher.mp_overview_data
+
+    insert_party_data(conn, cursor,)
+    insert_mp_data(conn, cursor)
 
 
 def bill_id_in_bills_table(conn, cursor, bill):
@@ -174,52 +195,54 @@ def execute_insert_new_vote_into_mpvotes_table(cursor, division_title, stage, bi
     cursor.execute(insert_command_string)
 
 
-
-def execute_bill_data_in_db(conn, cursor, bills_overview):
+def execute_bill_and_division_data_in_db(conn, cursor, bills_overview):
     for bill in bdi.get_bill_details(bills_overview):
         # get the billID from DB
         bill_id = bill_id_in_bills_table(conn, cursor, bill)
         # if there is no billID, then the bill is not in the table, hence we need to insert the bill
         if bill_id is None:
+            print(f"bill {bill.title_stripped} not yet in Bills table")
             insert_new_bill_into_bills_table(conn, cursor, bill)
             bill_id = bill_id_in_bills_table(conn, cursor, bill)
+        else:
+            print(f"bill {bill.title_stripped} already in Bills table")
         # todo: otherwise, modify the row to put in the data which may have changed (we dont know what has changed, so
         #  insert all of it
-        else:
-            pass
 
         for division in bill.divisions_list:
             division_title = division.division_name
             division_stage = division.division_stage
             # only insert if we haven't already inserted the results
             if not division_in_mpvotes_table(conn, cursor, division.division_name):
-                print(f"division {division_title} not in MPVotes table")
+                print(f"division {division_title} not yet in MPVotes table")
                 for no in division.noes:
                     execute_insert_new_vote_into_mpvotes_table(cursor, division_title, division_stage, bill_id, no, aye=False)
                 for aye in division.ayes:
                     execute_insert_new_vote_into_mpvotes_table(cursor, division_title, division_stage, bill_id, aye, aye=True)
-                conn.commit()
+            else:
+                print(f"division {division_title} already in MPVotes table")
+
+            conn.commit()
 
 
-def insert_bills_and_divisions_data(conn, cursor):
+def insert_bills_and_divisions_data(conn, cursor, fresh=False, session="2019-21"):
+    if fresh == True:
+        os.remove("datetime_last_scraped.p")
+        clear_table(conn, cursor, "MPVotes")
+        clear_table(conn, cursor, "Bills")
+
+
     bills_overview = blf.BillsOverview()
-    # todo in final version the session_name must be "All" - but check the script works on Google cloud first
-    bills_overview.update_all_bills_in_session(session_name="2019-21")
+    bills_overview.get_changed_bills_in_session(session_name=session)
+    print("new bills_overview.bills_overview_data")
+    print(bills_overview.bills_overview_data)
     print("bills overview object obtained, scraping complete")
-    print(f"{bills_overview.bills_overview_data.count()} items")
+    print(f"item counts: {bills_overview.bills_overview_data.count()}")
 
     # insert everything back in
-    execute_bill_data_in_db(conn, cursor, bills_overview)
+    execute_bill_and_division_data_in_db(conn, cursor, bills_overview)
 
     conn.commit()
-
-
-def refresh_mp_and_party_tables(conn, cursor):
-    clear_table(conn, cursor, "MP")
-    clear_table(conn, cursor, "Party")
-    insert_party_data(conn, cursor)
-    insert_missing_party_data(conn, cursor)
-    insert_mp_data(conn, cursor)
 
 
 # function called by cron, I need to split functionality into different functions
@@ -230,13 +253,14 @@ def insert_and_update_data():
     # todo: only run this infrequently
     # clear the tables in order according to foreign key constraints, then add all values back in
     # commented out - data currently in db, working on inserting bill and division data
-    refresh_mp_and_party_tables(conn, cursor)
-    print("finished updating MP and Party table")
+    #refresh_mp_and_party_tables(conn, cursor)
+    #print("finished updating MP and Party table")
 
     print_all_rows_of_table(cursor, "MP")
     print_all_rows_of_table(cursor, "Party")
 
-    insert_bills_and_divisions_data(conn, cursor)
+    # todo in final version the session_name must be "All" - but check the script works on Google cloud first
+    insert_bills_and_divisions_data(conn, cursor, fresh=True, session="2019-21")
 
     cursor.close()
     conn.close()
