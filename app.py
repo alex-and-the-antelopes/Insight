e6b23c0aa27ea9500fa822ad7334ffb6c0a3345e
sgavr <sg2295@bath.ac.uk>
2021-03-21 19:25:19 +0200

    feat: add check for unique tokens

--- app.py
+++ app.py
@@ -210,7 +210,9 @@ def create_session_token():
     """
     token = ''.join(random.SystemRandom().choice(string.digits + string.ascii_lowercase + string.ascii_uppercase)
                     for _ in range(8))  # Use digits, lowercase and uppercase letters, length 8
-    # Look if it's unique i.e. does not appear already in the db (if not repeat the process) todo
+    # Look if it's unique i.e. does not appear already in the db (if not repeat the process)
+    if database.interact(f"SELECT * FROM Users WHERE sessionToken='{token}';"):
+        return create_session_token()
     return token
 
 
