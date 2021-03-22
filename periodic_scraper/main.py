651ecd4b0d2048087ad25b946daec1a212b503cc
Robert <1robertchambers@gmail.com>
2021-03-22 14:59:29 +0000

    refactor: remove unneeded funcs from main.py

--- periodic_scraper/main.py
+++ periodic_scraper/main.py
@@ -1,16 +1,5 @@
 import periodic_scraper as ps
 ps.insert_and_update_data()
-import google.cloud.logging
-import logging
-
-def test_logging_func():
-    logging_client = google.cloud.logging.Client()
-
-    logging_client.get_default_handler()
-    logging_client.setup_logging(log_level=logging.INFO)
-
-    test_msg = "TEST MESSAGE"
-    logging.warning(test_msg)
 
 
 def main(data, context):
