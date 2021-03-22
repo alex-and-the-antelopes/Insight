e31ba4c970c60d33a17873ad0eafa1cfe3964a06
Robert <1robertchambers@gmail.com>
2021-03-22 00:32:56 +0000

    test: add log level severity

--- periodic_scraper/periodic_scraper.py
+++ periodic_scraper/periodic_scraper.py
@@ -399,7 +399,7 @@ def test_logging_func():
     logging_client = google.cloud.logging.Client()
 
     logging_client.get_default_handler()
-    logging_client.setup_logging()
+    logging_client.setup_logging(log_level=logging.INFO)
 
     test_msg = "TEST MESSAGE"
     logging.warning(test_msg)
