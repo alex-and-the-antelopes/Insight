1dd6d9098e7ce6797a4a1787569b7e5b77eb78be
sgavr <sg2295@bath.ac.uk>
2021-03-25 01:57:17 +0200

    fix: typecast date to str

--- app.py
+++ app.py
@@ -131,8 +131,8 @@ def get_mp_bills():
 
     bill_list = []  # Holds the list of bills to be transmitted
     for bill_data in bill_query:  # Put all bills in the query in the correct format
-        bill = core.Bill(bill_data[0], bill_data[1], None, bill_data[3][:10].replace(" ", ""), None, None, bill_data[2],
-                         link=bill_data[4])
+        bill = core.Bill(bill_data[0], bill_data[1], None, str(bill_data[3])[:10].replace(" ", ""), None, None,
+                         bill_data[2], link=bill_data[4])
         bill_dict = bill.to_dict()  # Get the dictionary representation of the bill
         bill_dict['likes'] = random.randint(0, 4)
         bill_dict['dislikes'] = random.randint(0, 4)
