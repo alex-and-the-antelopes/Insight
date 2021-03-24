2510b04460d6f2575f87e33f05453eed548dca4a
Robert <1robertchambers@gmail.com>
2021-03-24 08:16:36 +0000

    feat: set update frequency for mp + party data

--- periodic_scraper/main.py
+++ periodic_scraper/main.py
@@ -2,5 +2,6 @@ import periodic_scraper as ps
 
 
 def main(data, context):
-    ps.insert_and_update_data(day_frequency_for_party_and_mp_data=1)
+    reasonable_mp_update_frequency = 5
+    ps.insert_and_update_data(day_frequency_for_party_and_mp_data=reasonable_mp_update_frequency)
 
