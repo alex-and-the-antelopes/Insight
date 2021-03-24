8f0306e0c4dff9ff575781fa5f028f478a706e64
Robert <1robertchambers@gmail.com>
2021-03-24 08:27:11 +0000

    test: update mps

--- periodic_scraper/main.py
+++ periodic_scraper/main.py
@@ -2,6 +2,6 @@ import periodic_scraper as ps
 
 
 def main(data, context):
-    reasonable_mp_update_frequency = 5
+    reasonable_mp_update_frequency = 1
     ps.insert_and_update_data(day_frequency_for_party_and_mp_data=reasonable_mp_update_frequency)
 
