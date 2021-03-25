9b91b8402aae62ad07b349d2e359b64c55b5d3df
sgavr <sg2295@bath.ac.uk>
2021-03-25 21:00:08 +0200

    docs: add comments and docstrings for notifications

--- notifications.py
+++ notifications.py
@@ -6,32 +6,31 @@ from exponent_server_sdk import (
     PushServerError,
 )
 from requests.exceptions import ConnectionError, HTTPError
-import sys
 from bill_tracker_core import User
+import sys
 
 
-def send_notification_to_clients(clients, title, body):
+def send_notification_to_clients(clients: list, title: str, body: str) -> None:
     """
-    Send a notification with the given title and body to the client.
-    :param clients: list of clients to send the notification to (List of Users)
-    :param title: title of the notification (str)
-    :param body: body of the notification (str)
+    Send a notification with the given title and body to the client. Can raise TypeError.
+    :param clients: The list of users to send the notification to. Should be a list of User objects.
+    :param title: The title of the notification.
+    :param body: The body of the notification.
     """
     if type(clients) is not list:  # Check that a list is passed
         raise TypeError("Expected type <class 'list'> got type ", type(clients))
 
     for client in clients:  # Loop through each client in the list
         send_notification(client, title, body)  # Send message to each client
-
     return
 
 
-def send_notification(client, title, body):
+def send_notification(client: User, title: str, body: str) -> None:
     """
-    Send a notification with the given title and body to the client.
-    :param client: intended recipient of the notification to (User)
-    :param title: title of the notification (str)
-    :param body: body of the notification (str)
+    Send a notification with the given title and body to the client. Can raise TypeError.
+    :param client: The intended recipient of the notification to.
+    :param title: The title of the notification.
+    :param body: The body of the notification.
     """
     if type(client) is not User:  # Check that an instance of User is passed
         raise TypeError("Expected type <class 'User'> got type ", type(client))
@@ -46,20 +45,19 @@ def send_notification(client, title, body):
     except (ConnectionError, HTTPError):  # Encountered some Connection or HTTP error - retry a few times in
         print("Connection or HTTP error", file=sys.stderr)
     except DeviceNotRegisteredError:  # Device token is outdated, or wrong
-        print("Device not registered", file=sys.stderr)  # Todo: Should remove device (or ignore), or request new token
+        print("Device not registered", file=sys.stderr)
     except PushResponseError:  # Did not deliver the notification
-        print("PushResponseError")
-
+        print("PushResponseError", file=sys.stderr)
     return
 
 
-def build_notification(destination, title, body):
+def build_notification(destination: str, title: str, body: str) -> PushMessage:
     """
-    Creates and returns a valid notification, ready to be broadcast
-    :param destination: expo client notification token (str)
-    :param title: notification title (str)
-    :param body: main body of the notification (str)
-    :return: built notification to broadcast (PushMessage)
+    Creates and returns a valid notification, ready to be broadcast. Can raise TypeError and ValueError.
+    :param destination: expo client notification token.
+    :param title: The notification's title.
+    :param body: The main body of the notification.
+    :return: The built notification to broadcast.
     """
     if type(destination) is not str or type(title) is not str or type(body) is not str:  # Check for type errors
         raise TypeError("Expected type <class 'str'>")
