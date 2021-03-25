112c7a8e3d2d88c5a8a2e2eba32737bc949c98c3
sgavr <sg2295@bath.ac.uk>
2021-03-26 00:18:01 +0200

    docs: add docstrings and todo for db_interactions

--- db_interactions.py
+++ db_interactions.py
@@ -2,20 +2,24 @@ import sqlalchemy
 import secret_manager as secret
 
 
-def init_tcp_connection_engine(db_config):
+def init_tcp_connection_engine(db_config: dict):
+    # Todo add docstring and method signature (return type) i know it's None or conn.execute
+    """
+
+    :param db_config:
+    :return:
+    """
     db_user = secret.get_version("db_user")
     db_pass = secret.get_version("db_pass")
     db_name = secret.get_version("db_name")
     db_host = secret.get_version("db_host")
 
-
-    # Extract host and port from db_host
+    # Extract the host and port from db_host
     host_args = db_host.split(":")
     db_hostname, db_port = host_args[0], int(host_args[1])
 
     pool = sqlalchemy.create_engine(
-        # Equivalent URL:
-        # mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
+        # Equivalent URL: mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
         sqlalchemy.engine.url.URL(
             drivername="mysql+pymysql",
             username=db_user,  # e.g. "my-database-user"
@@ -30,6 +34,11 @@ def init_tcp_connection_engine(db_config):
 
 
 def init_connection_engine():
+    # Todo add docstring and method signature (return type) i know it's None or conn.execute
+    """
+
+    :return:
+    """
     db_config = {
         # Pool size is the maximum number of permanent connections to keep.
         "pool_size": 5,
@@ -52,7 +61,13 @@ def init_connection_engine():
 db = init_connection_engine()
 
 
-def interact(statement):
+def interact(statement: str):
+    # Todo add docstring and method signature (return type) i know it's None or conn.execute
+    """
+    Executes the given SQL statement. Used for INSERT, DELETE, UPDATE SQL functions.
+    :param statement: The SQL statement to carry out.
+    :return:
+    """
     try:
         # Using a with statement ensures that the connection is always released
         # back into the pool at the end of statement (even if an error occurs)
@@ -62,24 +77,23 @@ def interact(statement):
         # If something goes wrong, handle the error in this section. This might
         # involve retrying or adjusting parameters depending on the situation.
         # [START_EXCLUDE]
-        raise RuntimeWarning("Interaction database failed with message:" + str(e))
+        raise RuntimeWarning(f"Interaction database failed with message: {str(e)}")
 
 
-def select(statement):
-    """ Special function for select or similar statements which require something to be returned from the db
-    as we want to return a value.
-    :param statement: The statement to be carried out.
-    :return: The resulting query from the database.
+def select(statement: str) -> list or None:
     """
-
+    Executes the given SQL statement. Used for SELECT and similar functions that require something to be returned from
+    the database.
+    :param statement: The SQL statement to be carried out.
+    :return: The resulting query from the database in the form of a list.
+    """
     try:
         # Using a with statement ensures that the connection is always released
         # back into the pool at the end of statement (even if an error occurs)
         with db.connect() as conn:
-            return list(conn.execute(statement).fetchall())
+            return list(conn.execute(statement).fetchall())  # Return the result as a list
     except Exception as e:
         # If something goes wrong, handle the error in this section. This might
         # involve retrying or adjusting parameters depending on the situation.
         # [START_EXCLUDE]
-        raise RuntimeWarning("Selection database failed with message:" + str(e))
-
+        raise RuntimeWarning(f"Selection database failed with message: {str(e)}")
