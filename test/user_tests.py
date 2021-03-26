bb8b67552e9e5e289b9ce036792b41d10ec35199
sgavr <sg2295@bath.ac.uk>
2021-03-26 20:04:29 +0200

    tests: remove deprecated tests

--- test/user_tests.py
+++ test/user_tests.py
@@ -41,31 +41,3 @@ class UserTestCases(unittest.TestCase):
         self.assertFalse(user.verify_token(None))
         self.assertFalse(user.verify_token(("AhfO3sd", "AhfO3sd")))
         self.assertFalse(user.verify_token(13.012))
-
-    def test_to_dict(self):
-        """
-        Test the to_dict() in User todo fix to_dict tests
-        """
-        user = User("admin", "pass", "n token")
-        user_dict = user.to_dict()
-        # Check for actual value:
-        self.assertEqual({'username': 'admin', 'password_hash': "password_hash", 'notification_token': "n token"}, user_dict)
-        # Check for wrong value:
-        self.assertNotEqual({'username': 'admin', 'password_hash': "password_hash", 'notification_token': "n token"}, user_dict)  # Correct name, wrong pass, correct token
-        self.assertNotEqual({'username': 'Admin', 'password_hash': "password_hash", 'notification_token': "n token"}, user_dict)  # Wrong name, correct pass, correct token
-        self.assertNotEqual({'username': 'admin', 'password_hash': "password_hash", 'notification_token': "N token"}, user_dict)  # Correct name, correct pass, wrong token
-        self.assertNotEqual({'username': 'Admin', 'password_hash': "password_hash", 'notification_token': "N token"}, user_dict)  # All wrong values
-        # Check Wrong Types:
-        # Partially wrong types:
-        self.assertNotEqual({'username': None, 'password_hash': "password_hash", 'notification_token': "n token"}, user_dict)  # None name
-        self.assertNotEqual({'username': "admin", 'password_hash': None, 'notification_token': "n token"}, user_dict)  # None pass
-        self.assertNotEqual({'username': "admin", 'password_hash': "password_hash", 'notification_token': None}, user_dict)  # None token
-        self.assertNotEqual({'username': None, 'password_hash': None, 'notification_token':None}, user_dict)  # None all
-        # Fully wrong types:
-        self.assertNotEqual("user", user_dict)
-        self.assertNotEqual(None, user_dict)
-        self.assertNotEqual(13.012, user_dict)
-
-
-if __name__ == '__main__':
-    unittest.main()
